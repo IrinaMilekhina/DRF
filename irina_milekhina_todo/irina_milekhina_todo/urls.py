a6000b9c2a40c8778b3b944668f0fe488b44a332
i.milekhina <i.mezhova@gmail.com>
2021-08-26 18:34:44 +0300

    dz_1 creating api

--- irina_milekhina_todo/irina_milekhina_todo/urls.py
+++ irina_milekhina_todo/irina_milekhina_todo/urls.py
@@ -1,4 +1,4 @@
-"""irina_milekhina_todo URL Configuration
+"""todo_old URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from users.views import UsersViewSet
+
+router = DefaultRouter()
+router.register('users', UsersViewSet)
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
