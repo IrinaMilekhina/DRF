5e543916f75945a575854a5451d3018ce6160f9b
i.milekhina <i.mezhova@gmail.com>
2021-09-06 17:45:24 +0300

    dz_4 Задан размер страницы для всех api 100 записей. Настроен постраничный вывод.

--- irina_milekhina_todo/todos/views.py
+++ irina_milekhina_todo/todos/views.py
@@ -1,15 +1,26 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 from todos.filters import TodoFilter
 from todos.models import Project, Todo
 from todos.serializers import ProjectModelSerializer, TodoModelSerializer
 
 
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 5
+
+
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class TodoViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
+    pagination_class = TodoLimitOffsetPagination
     filterset_class = TodoFilter
