a9acb5d3181e018b72912e95f8bee52eaeec821f
i.milekhina <i.mezhova@gmail.com>
2021-09-13 15:01:14 +0300

    dz_6 Cистема прав по группам, базовая авторизация, авторизация по токену, jwt

--- irina_milekhina_todo/irina_milekhina_todo/settings.py
+++ irina_milekhina_todo/irina_milekhina_todo/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     # 3-d part apps
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'django_filters',
     # custom apps
     'users',
@@ -146,4 +148,12 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ]
 }
